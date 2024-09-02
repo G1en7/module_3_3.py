74d99642c7c1cf356fd5dc7ae21c04384e76bcc9
G1en7 <belkin.glebaty@mail.ru>
2024-09-02 16:13:46 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,10 +1,22 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
+
+
+print_params(32)
+print_params(32, 'Gleb')
+print_params(32, 'Gleb', False)
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+
 values_list = (23, 'one', False)
 values_dict = {'a': 1, 'b' : 'строка', 'c': True}
 
+
 print_params(*values_list)
 print_params(**values_dict)
+
 
 values_list_2 = [33.42, 'студент']
 print_params(*values_list_2, 54)
